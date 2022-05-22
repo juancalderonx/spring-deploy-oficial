55469b4b19b873ca3c175064bc740f4eb4fe249f
juancalderonx <juanescalderon12@gmail.com>
2022-05-22 12:40:42 -0500

    Initial commit

---
+++ src/main/java/Package/SpringDeployOficialApplication.java
@@ -0,0 +1,13 @@
+package Package;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDeployOficialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDeployOficialApplication.class, args);
+    }
+
+}
